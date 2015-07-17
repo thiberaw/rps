f0dd1b081875bfae4f0790f5422a2f13b7062350
Thibaut Ware <thibautware@outlook.com>
2015-07-17 17:41:12 +0200

    clean & correct getRandInt

--- rps.js
+++ rps.js
@@ -11,25 +11,31 @@
     var rps = {
         getRandInt: function() {
 
-            return Math.floor(Math.random() * (3 - 1)) + 1;
+            return Math.floor(Math.random() * (4 - 1)) + 1;
 
         },
         getResult: function(result){
 
-            if(result === '12' || result === '23' || result === '31'){
-                count_box.innerText = 'lost';
-            }else if(result === '13' || result === '21' || result === '32'){
-                count_box.innerText = 'win';
-            }else{
-                count_box.innerText = 'tie game';
+            if (result === '12' || result === '23' || result === '31') {
+                
+                return 'lost';
+
+            } else if (result === '13' || result === '21' || result === '32') {
+                
+                return 'win';
+
+            } else {
+                
+                return 'tie game';
+
             }
 
         },
-        setRpsClass: function(elem,num){
+        setRpsClass: function(num){
 
             var rps_class   = ['rps_rock_cp','rps_paper_cp','rps_scissors_cp'];
 
-            elem.className = 'rps_choice '+rps_class[num-1];
+            return rps_class[num-1];
 
         }
     };
@@ -41,10 +47,10 @@
             var cp_num = rps.getRandInt();
             var user_choice = idx + 1;
 
-            var result = user_choice + '' +cp_num;
+            var result = user_choice + '' + cp_num;
 
-            rps.setRpsClass(cp_choice, cp_num)
-            rps.getResult(result);
+            cp_choice.className = 'rps_choice ' + rps.setRpsClass(cp_num);
+            count_box.innerText = rps.getResult(result);
 
         }, false);
 
