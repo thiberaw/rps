cb6b68ff1f146f7ac1371fd0d35b63f8634e4e3e
Thibaut Ware <thibautware@outlook.com>
2015-07-17 16:50:14 +0200

    delete time, count, score

--- rps.js
+++ rps.js
@@ -4,28 +4,12 @@
 var rps_tab         = document.getElementsByClassName('rps_choice'),
     cp_choice       = document.getElementById('cp_choice'),
     count_box       = document.getElementById('count_box'),
-    count           = 0,
     game_on         = 0,
     result_tab      = [],
     rps_time,
     cp_num;
 
 var rps = {
-    countTime: function(){
-        var rps_word    = ['ick','ack','ock !'];
-
-        count_box.innerText = rps_word[count];
-
-        count++;
-        rps_time = setTimeout(function(){rps.countTime();},1100);
-
-        if(count == 3){
-            clearTimeout(rps_time);
-
-            rps.getRandInt(1,4);
-            setTimeout(function(){count = 0;},300);
-        }
-    },
     getRandInt: function(min, max) {
         cp_num = Math.floor(Math.random() * (max - min)) + min;
 
@@ -57,52 +41,9 @@ var rps = {
         var rps_class   = ['rps_rock_cp','rps_paper_cp','rps_scissors_cp'];
 
         elem.className = 'rps_choice '+rps_class[rpsClass-1];
-    },
-    getLocalStorage: function(key){
-        var keyStorage = localStorage.getItem(key);
-
-        if(keyStorage != null){
-            keyStorage = parseInt(keyStorage);
-        }else{
-            keyStorage = 0;
-        }
-
-        return keyStorage;
-    },
-    setLocalStorage: function(key, value){
-        localStorage.setItem(key,value);
-    },
-    writeScore: function(elem,score){
-        for(var i = 1; i <= score; i++){
-            rps.setMark(elem,i);
-        }
-    },
-    setMark: function(elem,num){
-        if(num % 5 == 0 && num != 0){
-            elem.innerHTML += '<span class="fifth_mark">|</span>';
-        }else {
-            elem.innerHTML += '|';
-        }
-    },
-    clean: function(){
-        result_tab = [];
-        count = 0;
-        game_on = 0;
-        cp_choice.innerText = '';
-        count_box.innerText = '';
     }
 };
 
-function timeout(par1,par2,timenum){
-    setTimeout(function(){
-        if(par1 == true){
-            rps.clean();
-        }
-        if(par2 == true){
-            start_rps.addEventListener('click', startRps, false);
-        }
-    },timenum);
-}
 
 for(var i = 0; i < 3; i++){
     rps_tab[i].addEventListener('click',function(){
@@ -135,6 +76,3 @@ for(var i = 0; i < 3; i++){
         }
     },false);
 }
-//show scores on page load
-rps.writeScore(cp_score,rps.getLocalStorage('cp_win'));
-rps.writeScore(user_score,rps.getLocalStorage('user_win'));
