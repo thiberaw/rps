3d8d7719c5a7468f962015a2c4e365c2c4a9013a
thiberaw <thibautware@outlook.com>
2016-01-29 00:18:04 +0100

    many changes : naming, functional

--- rps.js
+++ rps.js
@@ -6,29 +6,16 @@
 
     var rps_tab     = document.getElementsByClassName('rps_choice'),
         cp_choice   = document.getElementById('cp_choice'),
-        count_box   = document.getElementById('count_box'),
         rps_score   = document.getElementById('rps_score'),
         rps_score_cp= document.getElementById('rps_score_cp'),
-        rps_class   = ['rps_rock_cp','rps_paper_cp','rps_scissors_cp'],
-        results = {
-            '12' : 'lost',
-            '23' : 'lost',
-            '31' : 'lost',
-            '11' : 'draw',
-            '22' : 'draw',
-            '33' : 'draw',
-            '13' : 'win',
-            '21' : 'win',
-            '32' : 'win'
-    };
+        rps_class   = ['rps_rock_cp','rps_paper_cp','rps_scissors_cp'];
 
-    function setCpScore(){
+    function getCpScore(){
         return Math.floor(Math.random() * (4 - 1)) + 1;
     }
 
-    function displayResult(idx, cp_num){
-        var res = ( idx + 1 ) + '' + cp_num;
-        return results[res];
+    function getResult(idx, cp_num){
+        return ( idx + 1 ) - cp_num;
     }
 
     function classMatch(elem, cName){
@@ -38,41 +25,40 @@
         return false;
     }
 
-    function writeUserScore(res){
-        if(res !== 'draw'){
-            var elemArrLength, lastElement, winner;
+    function getWinner(res, callB){
+        if(res !== 0){
+            var winner;
             
-            if(res === 'win'){
+            if(res === 1 || res === -2){
                 winner = rps_score;
-            } else if (res === 'lost'){
+            } else {
                 winner = rps_score_cp;
             }
             
-            elemArrLength = winner.childElementCount - 1;
-            lastElement = winner.children[elemArrLength];
-            writeScore(winner,lastElement);
+            callB(winner);            
         }
     }
 
-    function writeScore(playerScore, lastElement){
-        if(classMatch(lastElement, 'fiveBars')){
-            playerScore.innerHTML += '<span>|</span>';
+    function writeScore(winner){
+        var lastElem = winner.lastChild;
+
+        if(classMatch(lastElem, 'fiveBars')){
+            winner.innerHTML += '<span>|</span>';
         } else {
-            if(lastElement.innerText === '||||'){
-                lastElement.className = 'fiveBars';
+            if(lastElem.innerText === '||||'){
+                lastElem.className = 'fiveBars';
             } else {
-                lastElement.innerText += '|';
+                lastElem.innerText += '|';
             }       
         }
     }
 
     function playGame(idx){
-        var cp_num = setCpScore();
-        var result = displayResult(idx, cp_num); 
+        var cp_num = getCpScore();
+        var result = getResult(idx, cp_num); 
 
         cp_choice.className = 'rps_choice ' + rps_class[cp_num - 1];
-        count_box.innerText = result; 
-        writeUserScore(result);
+        getWinner(result, writeScore);
 
     }
 
