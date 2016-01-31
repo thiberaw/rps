f7ff488261711b22dc75d5be4e1f2cc49d6ae6e8
thiberaw <thibautware@outlook.com>
2016-01-31 17:50:57 +0100

    change var names

--- rps.js
+++ rps.js
@@ -5,9 +5,9 @@
     'use strict';
 
     var rps_tab     = document.getElementsByClassName('rps_choice'),
-        rps_cp-choice   = document.getElementById('rps_cp-choice'),
-        rps_user-score   = document.getElementById('rps_user-score'),
-        rps_cp-score= document.getElementById('rps_cp-score'),
+        rps_cpChoice   = document.getElementById('rps_cp-choice'),
+        rps_userScore   = document.getElementById('rps_user-score'),
+        rps_cpScore= document.getElementById('rps_cp-score'),
         rps_class   = ['rps_rock_cp','rps_paper_cp','rps_scissors_cp'];
 
     function getCpScore(){
@@ -30,9 +30,9 @@
             var winner;
             
             if(res === 1 || res === -2){
-                winner = rps_user-score;
+                winner = rps_userScore;
             } else {
-                winner = rps_cp-score;
+                winner = rps_cpScore;
             }
             
             callB(winner);            
@@ -57,7 +57,7 @@
         var cp_num = getCpScore();
         var result = getResult(idx, cp_num); 
 
-        rps_cp-choice.className = 'rps_choice ' + rps_class[cp_num - 1];
+        rps_cpChoice.className = 'rps_choice ' + rps_class[cp_num - 1];
         getWinner(result, writeScore);
 
     }
